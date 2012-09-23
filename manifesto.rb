58457da2044f45e50da00f5a676a0ec0df626ff0
James Smith <james@floppy.org.uk>
2012-09-23 17:56:56 +0300

    Separate layout

--- manifesto.rb
+++ manifesto.rb
@@ -43,19 +43,3 @@ get '/logout' do
   session[:authenticated] = false
   redirect '/'
 end
-
-__END__
-
-@@ layout
-<html>
-  <head>
-    <link href='http://twitter.github.com/bootstrap/1.4.0/bootstrap.min.css' rel='stylesheet' />
-  </head>
-  <body>
-    <div class='container'>
-      <div class='content'>
-        <%= yield %>
-      </div>
-    </div>
-  </body>
-</html>
